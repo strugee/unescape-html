3cbdc3e15a6285fac60561608d9710e660c85238
AJ Jordan <alex@strugee.net>
2017-08-23 01:45:32 -0400

    Support `:` escaping

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ module.exports = function (html) {
   return String(html)
     .replace(/&quot;/g, '"')
     .replace(/&#39;/g, '\'')
+    .replace(/&#x3A;/g, ':')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&amp;/g, '&')
+  
 }
